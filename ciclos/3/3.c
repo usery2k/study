2619fb4f7119907f6322e4c66c715ba551cccafd
matias <matzzz423@gmail.com>
2024-06-29 19:09:53 -0400

    ejercicio 3

--- ciclos/3/3.c
+++ ciclos/3/3.c
@@ -25,6 +25,12 @@ int main(){
              }
              printf("\n");
         }
+         for (int i =inum - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+         }
 
     return 0;
 }
